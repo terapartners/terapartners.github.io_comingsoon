44a29ec633204cca030a9aad6d22515e80702378
Pedro Victor <pedro.victor@gmail.com>
2019-01-25 17:40:03 -0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tera Partners: Coming Soon
+
+Placeholder website for Tera Partners while v1 is under construction.
---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  AOS.init({
+      offset: 0,
+      duration: 600,
+      easing: "ease-out-expo"
+  })
+});
